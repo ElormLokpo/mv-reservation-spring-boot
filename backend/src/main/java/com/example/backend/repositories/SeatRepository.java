4628e4b1e889231ab8aa04c55bdb2f0fc5804342
ElormLokpo <benedictdev31@gmail.com>
2025-02-06 16:52:08 +0000

    changes

--- backend/src/main/java/com/example/backend/repositories/SeatRepository.java
+++ backend/src/main/java/com/example/backend/repositories/SeatRepository.java
@@ -1,5 +1,6 @@
 package com.example.backend.repositories;
 
+import java.util.Collection;
 import java.util.UUID;
 
 import org.springframework.data.domain.Page;
@@ -15,4 +16,7 @@ import com.example.backend.models.seats.SeatModel;
 public interface SeatRepository extends JpaRepository<SeatModel, UUID> {
     @Query("SELECT s from seats s WHERE s.theater.id = :theaterId")
     Page<SeatModel> findSeatsByTheater(@Param("theaterId") UUID theaterId, Pageable pageable);
+
+    @Query("SELECT s from seats s WHERE s.theater.id = :theaterId")
+    Collection<SeatModel> findSeatsByTheaterR(@Param("theaterId") UUID theaterId);
 }
--- backend/src/main/java/com/example/backend/services/ShowtimeService.java
+++ backend/src/main/java/com/example/backend/services/ShowtimeService.java
@@ -2,6 +2,7 @@ package com.example.backend.services;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
 import java.util.UUID;
 import java.util.stream.Collectors;
 import org.springframework.data.domain.Page;
@@ -15,9 +16,11 @@ import com.example.backend.dtos.showtime.CreateShowtimeDto;
 import com.example.backend.dtos.showtime.GetShowtimeDto;
 import com.example.backend.exceptions.ResourceNotfoundException;
 import com.example.backend.mappers.ShowtimeMapper;
+import com.example.backend.models.seats.SeatModel;
 import com.example.backend.models.showtime.ShowtimeModel;
 import com.example.backend.models.theater.TheaterModel;
 import com.example.backend.models.tickets.TicketModel;
+import com.example.backend.repositories.SeatRepository;
 import com.example.backend.repositories.ShowtimeRepository;
 import com.example.backend.repositories.TicketRepository;
 
@@ -27,11 +30,14 @@ public class ShowtimeService implements ShowtimeDao {
         ShowtimeRepository showtimeRepository;
         TheaterService theaterService;
         TicketRepository ticketRepository;
+        SeatRepository seatRepository;
 
-        public ShowtimeService(ShowtimeRepository _showtimeRepository, TheaterService _theaterService, TicketRepository _ticketRepository) {
+        public ShowtimeService(ShowtimeRepository _showtimeRepository, TheaterService _theaterService,
+                        TicketRepository _ticketRepository, SeatRepository _seatRepository) {
                 showtimeRepository = _showtimeRepository;
                 theaterService = _theaterService;
                 ticketRepository = _ticketRepository;
+                seatRepository = _seatRepository;
         }
 
         @Override
@@ -130,11 +136,15 @@ public class ShowtimeService implements ShowtimeDao {
 
                 ShowtimeModel showtimeCreated = showtimeRepository.save(showtime);
 
+                List<UUID> seatIds = seatRepository.findSeatsByTheaterR(theaterModel.getId()).stream()
+                                .map(SeatModel::getId).collect(Collectors.toList());
+
                 for (int i = 1; i <= theaterModel.getSeatingCapacity(); i++) {
                         tickets.add(
                                         TicketModel.builder()
                                                         .price(showtimeDto.getRate())
                                                         .showtime(showtimeCreated)
+                                                        .seat(seatIds.get(i))
                                                         .build());
                 }
 
